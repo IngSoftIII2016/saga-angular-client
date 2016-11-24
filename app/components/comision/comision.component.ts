8fe657f5cb30ab4999be84274b247ec5981a1c1e
JavierJV <valsecchi_javier@hotmail.com>
2016-11-24 17:22:49 -0300

    amb parcial comision

--- app/components/comision/comision.component.ts
+++ app/components/comision/comision.component.ts
@@ -1,7 +1,11 @@
 import {Component} from '@angular/core';
 import {Comision} from "../../entities/comision";
-import {Message, ConfirmationService} from "primeng/components/common/api";
+import {Message, ConfirmationService, SelectItem} from "primeng/components/common/api";
 import {ComisionStore} from "../../services/comision.store";
+import {AsignaturaService} from "../../services/asignatura.service";
+import {Asignatura} from "../../entities/asignatura";
+import {PeriodoService} from "../../services/periodo.service";
+import {Periodo} from "../../entities/periodo";
 import {Subject} from "rxjs";
 
 
@@ -9,7 +13,7 @@ import {Subject} from "rxjs";
     templateUrl: 'app/components/comision/comision.component.html',
     styleUrls: ['app/resources/demo/css/dialog.css'],
     selector: 'comision',
-    providers:[ComisionStore, ConfirmationService]
+    providers:[ComisionStore, AsignaturaService, PeriodoService, ConfirmationService]
 })
 export class ComisionComponent {
 
@@ -21,11 +25,42 @@ export class ComisionComponent {
 
     isNew: boolean;
 
+    asignatura: SelectItem[] = [];
+
+    asignaturaSelected: SelectItem;
+
+    periodo: SelectItem[] = [];
+
+    periodoSelected: SelectItem;
+
     private searchTerms = new Subject<string>();
 
-    constructor(private comisionStore: ComisionStore,  private confirmationService : ConfirmationService) { }
+    constructor(private comisionStore: ComisionStore,
+                private asignaturaService: AsignaturaService,
+                private periodoService: PeriodoService,
+                private confirmationService : ConfirmationService) { }
 
     ngOnInit() {
+        var sel = this;
+        this.asignaturaService.getAll().subscribe(asignatura => {
+            asignatura.forEach(asignatura => {
+                    sel.asignatura.push({
+                            label: asignatura.nombre, value: new Asignatura(asignatura)
+                        }
+                    )
+                }
+            )
+        });
+        var sel = this;
+        this.periodoService.getAll().subscribe(periodo => {
+            periodo.forEach(periodo => {
+                    sel.periodo.push({
+                            label: periodo.descripcion, value: new Periodo(periodo)
+                        }
+                    )
+                }
+            )
+        });
         this.searchTerms
             .debounceTime(300)
             .distinctUntilChanged()
@@ -42,27 +77,72 @@ export class ComisionComponent {
     onRowSelect(event) {
         this.isNew = false;
         this.comision =new Comision(event.data);
+        this.asignaturaSelected = {label: this.comision.asignatura.nombre, value: new Asignatura(this.comision.asignatura)};
+        this.periodoSelected = {label: this.comision.periodo.descripcion, value: new Periodo(this.comision.periodo)};
         this.displayDialog = true;
     }
 
     save() {
-        this.comisionStore.save(this.comision).subscribe(
-            guardada => {
-                this.displayDialog = false;
-                this.msgs.push(
-                    {
-                        severity:'success',
-                        summary:'Guardado',
-                        detail:'Se ha guardado la clase con exito!'
-                    })
-            },
-            error => {
-                this.msgs.push(
-                    {
-                        severity:'error',
-                        summary:'Error',
-                        detail:'No se ha podido guardar la clase:\n' + error
-                    });
+        if (this.comision.asignatura.nombre != this.asignaturaSelected.label){
+            this.comision.asignatura = new Asignatura(this.asignaturaSelected);
+        }
+        if (this.comision.periodo.descripcion != this.periodoSelected.label){
+            this.comision.periodo = new Periodo(this.periodoSelected);
+        }
+        if (this.isNew) {
+            this.comision.asignatura = new Asignatura(this.asignaturaSelected);
+            this.comision.periodo = new Periodo(this.periodoSelected);
+            this.confirmationService.confirm({
+                message: 'Estas seguro que desea agregar el aula?',
+                header: 'Confirmar ',
+                icon: 'fa fa-plus-square',
+                accept: () => {
+                    this.comisionStore.create(this.comision).subscribe(
+                        creada => {
+                            this.displayDialog = false;
+                            this.msgs.push(
+                                {
+                                    severity: 'success',
+                                    summary: 'Creada',
+                                    detail: 'Se ha agregado el aula ' + creada.nombre + ' con exito!'
+                                })
+                        },
+                        error => {
+                            this.msgs.push(
+                                {
+                                    severity: 'error',
+                                    summary: 'Error',
+                                    detail: 'No se ha podido crear el aula:\n' + error
+                                });
+                        });
+                }
+            });
+        }
+        else
+            this.confirmationService.confirm({
+                message: 'Estas seguro que desea agregar el aula?',
+                header: 'Confirmar ',
+                icon: 'fa fa-plus-square',
+                accept: () => {
+                    this.comisionStore.create(this.comision).subscribe(
+                        creada => {
+                            this.displayDialog = false;
+                            this.msgs.push(
+                                {
+                                    severity: 'success',
+                                    summary: 'Creada',
+                                    detail: 'Se ha agregado el aula ' + creada.nombre + ' con exito!'
+                                })
+                        },
+                        error => {
+                            this.msgs.push(
+                                {
+                                    severity: 'error',
+                                    summary: 'Error',
+                                    detail: 'No se ha podido crear el aula:\n' + error
+                                });
+                        });
+                }
             });
     }
 
